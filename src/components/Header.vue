52cb27d3961abb9019690ceff1cc3d78af770279
Cezar Juventino Martins Neto <103068760+CezarMNeto@users.noreply.github.com>
2022-09-26 15:41:09 -0300

    inicio do header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { defineComponent } from 'vue'
+        
+    export default defineComponent({
+        data(){
+            return {
+                resultado: []
+            }
+        }
+    });
+</script>
+<template>
+    <header class="container-header">
+        <div class="principal">
+            <img src="../assets/imagens/logo-horizontal.svg" alt="logo devagram" />
+            <div class="group">
+                <div class="pesquisa">
+                    <img src="../assets/imagens/busca.svg" alt="pesquisa" layout="fill" class="icon" />
+                    <input type="text" placeholder="Pesquisar" />
+                </div>
+                <!-- Navegação-->
+            </div>
+        </div>
+        <div v-if="resultado.length > 0">
+            <!-- Resultado da busca -->
+        </div>
+    </header>
+</template>
+
+<style lang="scss" src="@/assets/styles/inputImagem.scss" />
