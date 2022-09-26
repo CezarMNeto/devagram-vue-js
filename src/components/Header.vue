979b17701980160e1d2939eabd55ac6307ec78dd
Cezar Juventino Martins Neto <103068760+CezarMNeto@users.noreply.github.com>
2022-09-26 17:52:32 -0300

    estrutura da postagem

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,34 @@
 <script lang="ts">
-    import { defineComponent } from 'vue'
-        
-    export default defineComponent({
-        data(){
-            return {
-                resultado: []
-            }
+import { defineComponent } from 'vue'
+import Navegacao from './Navegacao.vue';
+
+export default defineComponent({
+    data() {
+        return {
+            resultado: [],
+            inputFocus: false
         }
-    });
+    },
+    methods: {
+        setFocus(v: boolean) {
+            this.inputFocus = v;
+        }
+    },
+    components: { Navegacao }
+});
 </script>
+
 <template>
     <header class="container-header">
         <div class="principal">
             <img src="../assets/imagens/logo-horizontal.svg" alt="logo devagram" />
             <div class="group">
-                <div class="pesquisa">
+                <div class="pesquisa" :class="{focus:inputFocus}">
                     <img src="../assets/imagens/busca.svg" alt="pesquisa" layout="fill" class="icon" />
-                    <input type="text" placeholder="Pesquisar" />
+                    <input type="text" placeholder="Pesquisar" @focus="setFocus(true)" @blur="setFocus(false)" />
                 </div>
-                <!-- Navegação-->
+
+                <Navegacao />
             </div>
         </div>
         <div v-if="resultado.length > 0">
@@ -27,4 +37,4 @@
     </header>
 </template>
 
-<style lang="scss" src="@/assets/styles/inputImagem.scss" />
+<style lang="scss" src="@/assets/styles/header.scss" />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
     import Header from '../components/Header.vue'
+    import Footer from '../components/Footer.vue'
+    import { FeedServices } from '../services/FeedServices';
+    import Feed from '../components/Feed.vue';
+    
+    const feedServices = new FeedServices();
 
     export default defineComponent({
-        components	: { Header }        
+        components: { Header, Footer, Feed },
+        data() {
+            return {
+                posts: [],
+                loading: false
+            }
+        },
+        async mounted() {
+            try {
+                this.loading = true;
+                const result = await feedServices.getFeedPrincipal();
+                if (result && result.data) {
+                    this.posts = result.data;
+                }
+            } catch (e) {
+                console.log(e);
+            }
+            this.loading = false;
+        }
     });
-</script>
-<template>
-    <header />
-</template>
+    </script>
+    <template>
+        <Loading :active="loading" :can-cancel="false" color="#5E49FF" :is-full-page="true"/>
+        <Header />
+        <Feed :posts="posts" v-if="posts && posts.length > 0" />
+        <Footer />
+    </template>
